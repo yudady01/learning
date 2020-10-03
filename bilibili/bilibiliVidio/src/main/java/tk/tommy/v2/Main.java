acd114d3a3d2f80a4d3ef765caef715ae431aefa
yudady <yudady@gmail.com>
2020-10-03 13:11:49 +0800

    bilibili v2

--- bilibili/bilibiliVidio/src/main/java/tk/tommy/v2/Main.java
+++ bilibili/bilibiliVidio/src/main/java/tk/tommy/v2/Main.java
@@ -5,24 +5,30 @@ import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
+    private static final String VIDIO_FOLDER = "C:/Users/yu_da/Downloads/bilibiliVidio/";
 
     public static void main(String[] args) throws Exception {
-        String vidioFolder = "C:/Users/yu_da/Downloads/bilibiliVidio/";
+
+
         String child = "667705991";
-        String title = CreateTitle.create(vidioFolder, child);
+        createOneViedo(child);
+    }
+
+
+    private static void createOneViedo(String child) {
+        String title = CreateTitle.create(VIDIO_FOLDER, child);
         String videoOutputFolder = "C:/Users/yu_da/OneDrive/Desktop/" + title;
         Paths.get(videoOutputFolder).toFile().mkdir();
 
-        CreateItemInfo.create(vidioFolder, child).forEach(item -> {
+        CreateItemInfo.create(VIDIO_FOLDER, child).forEach(item -> {
             String tmpMp4 = "C:/Users/yu_da/OneDrive/Desktop/viedo.mp4";
             FFmpegMediaUtil.videoConvert(item.inputs, tmpMp4);
 
             Path fileToMovePath = Paths.get(tmpMp4);
-            Path targetPath = Paths.get(videoOutputFolder + "/" + item.index + item.name + ".mp4");
+            String fileName = item.index + "-" + item.name + ".mp4";
+            Path targetPath = Paths.get(videoOutputFolder + "/" + fileName);
             try {
                 Files.move(fileToMovePath, targetPath);
             } catch (IOException e) {
@@ -32,13 +38,5 @@ public class Main {
         });
     }
 
-
-    private void tt() {
-        List<String> videoInputPaths = new ArrayList<>();
-        videoInputPaths.add("C:\\Users\\yu_da\\Downloads\\bilibiliVidio\\667705991\\1\\80\\audio.m4s");
-        videoInputPaths.add("C:\\Users\\yu_da\\Downloads\\bilibiliVidio\\667705991\\1\\80\\video.m4s");
-        String videoOutputPath = "C:\\Users\\yu_da\\OneDrive\\Desktop\\123\\newVideo.mp4";
-        FFmpegMediaUtil.videoConvert(videoInputPaths, videoOutputPath);
-    }
 
 }
